32ccf17619af078a87ed8cca503a092b924f8e0b
Jason Peck <pecjas@gmail.com>
2020-05-10 13:56:57 -0500

    Update PrioritizedTask's priority field to be enum, and update callers.

--- analyzer-core/src/main/java/ffb/analyzer/core/queue/PrioritizedTask.java
+++ analyzer-core/src/main/java/ffb/analyzer/core/queue/PrioritizedTask.java
@@ -3,25 +3,43 @@ package ffb.analyzer.core.queue;
 import java.util.function.Consumer;
 
 public class PrioritizedTask<T> extends Task<T> implements Comparable<PrioritizedTask<T>> {
-    private final Integer priority;
+    public enum Priority {
+        LOWER(10),
+        LOW(20),
+        MEDIUM(30),
+        HIGH(40),
+        HIGHER(50);
+
+        private final int value;
+
+        Priority(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return this.value;
+        }
+    }
+
+    private final Priority priority;
 
     @Override
     public int compareTo(PrioritizedTask<T> otherTask) {
-        return (this.getPriority() - otherTask.getPriority());
+        return (otherTask.getPriority().getValue() - this.getPriority().getValue());
     }
 
-    public PrioritizedTask(int id, int priority, T object, Consumer<T> action) {
+    public PrioritizedTask(int id, Priority priority, T object, Consumer<T> action) {
         super(object, action);
         this.id = id;
         this.priority = priority;
     }
 
-    public PrioritizedTask(int priority, T object, Consumer<T> action) {
+    public PrioritizedTask(Priority priority, T object, Consumer<T> action) {
         super(object, action);
         this.priority = priority;
     }
 
-    public Integer getPriority() {
+    public Priority getPriority() {
         return priority;
     }
 }
--- analyzer-core/src/test/java/ffb/analyzer/core/queue/PriorityQueueManagerTests.java
+++ analyzer-core/src/test/java/ffb/analyzer/core/queue/PriorityQueueManagerTests.java
@@ -5,6 +5,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import ffb.analyzer.core.queue.PrioritizedTask.Priority;
+
 public class PriorityQueueManagerTests {
     private static final int EXPECTED_QUEUE_DEPTH = 3;
     private static final int STARTING_VALUE = 5;
@@ -24,16 +26,16 @@ public class PriorityQueueManagerTests {
 
     @Test
     public void addToQueue() {
-        Assert.assertTrue(manager.addEntry(Utils.getExampleTask(15, exampleObject)));
-        Assert.assertTrue(manager.addEntry(Utils.getExampleTask(99999, exampleObject)));
-        Assert.assertTrue(manager.addEntry(Utils.getExampleTask(0, exampleObject)));
+        Assert.assertTrue(manager.addEntry(Utils.getExampleTask(Priority.HIGHER, exampleObject)));
+        Assert.assertTrue(manager.addEntry(Utils.getExampleTask(Priority.LOW, exampleObject)));
+        Assert.assertTrue(manager.addEntry(Utils.getExampleTask(Priority.HIGHER, exampleObject)));
 
         Assert.assertEquals(EXPECTED_QUEUE_DEPTH, manager.getQueue().size());
     }
 
     @Test
     public void performOneTask() {
-        Task<?> task = Utils.getExampleTask(5, exampleObject);
+        Task<?> task = Utils.getExampleTask(Priority.MEDIUM, exampleObject);
         Assert.assertEquals(exampleObject.value, STARTING_VALUE);
 
         task.performTask();
@@ -43,13 +45,13 @@ public class PriorityQueueManagerTests {
     @Test
     public void performInPriorityOrder() {
         Utils.ExampleObject objectOne = new Utils.ExampleObject(STARTING_VALUE);
-        PrioritizedTask<Utils.ExampleObject> taskOne = Utils.getExampleTask(1, objectOne);
+        PrioritizedTask<Utils.ExampleObject> taskOne = Utils.getExampleTask(Priority.HIGHER, objectOne);
 
         Utils.ExampleObject objectTwo = new Utils.ExampleObject(STARTING_VALUE);
-        PrioritizedTask<Utils.ExampleObject> taskTwo = Utils.getExampleTask(2, objectTwo);
+        PrioritizedTask<Utils.ExampleObject> taskTwo = Utils.getExampleTask(Priority.MEDIUM, objectTwo);
 
         Utils.ExampleObject objectThree = new Utils.ExampleObject(STARTING_VALUE);
-        PrioritizedTask<Utils.ExampleObject> taskThree = Utils.getExampleTask(3, objectThree);
+        PrioritizedTask<Utils.ExampleObject> taskThree = Utils.getExampleTask(Priority.LOW, objectThree);
 
         manager.addEntry(taskTwo);
         manager.addEntry(taskOne);
