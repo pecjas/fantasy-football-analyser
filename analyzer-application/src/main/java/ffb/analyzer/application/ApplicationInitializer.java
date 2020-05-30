4054bd4dadbcf59c2c51045fd4c7645173aadacf
David Hu <d.hu.development@gmail.com>
2020-05-30 10:05:02 -0500

    checkstyle

--- analyzer-application/src/main/java/ffb/analyzer/application/ApplicationInitializer.java
+++ analyzer-application/src/main/java/ffb/analyzer/application/ApplicationInitializer.java
@@ -1,31 +1,20 @@
 package ffb.analyzer.application;
 
-import javax.persistence.EntityManager;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 /**
  * Application initializer for the project.
  */
-//TODO: Add Web module once that's ready
 @SpringBootApplication(scanBasePackages = {"ffb.analyzer.beans"}, exclude = {
     DataSourceAutoConfiguration.class,
     DataSourceTransactionManagerAutoConfiguration.class,
     HibernateJpaAutoConfiguration.class
 })
 public class ApplicationInitializer {
-
-    @Autowired
-    EntityManager em;
 
     public static void main(String[] args) {
         SpringApplication.run(ApplicationInitializer.class);
